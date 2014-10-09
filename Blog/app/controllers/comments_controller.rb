4fc739763350c946717c004e6507a3736341717e
cyurtbil <cenky92@gmail.com>
2014-10-09 19:50:21 -0400

    Now you can add as many comments as you want

--- Blog/app/controllers/comments_controller.rb
+++ Blog/app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
 
-	def index
-		@comments = Comment.all
+	def create
+		@comment = Comment.create(comment_params)
+		redirect_to :back
 	end
+
+	private
+
+		def comment_params
+			params.require(:comment).permit(:comment, :post_id)
+		end
 
 end
