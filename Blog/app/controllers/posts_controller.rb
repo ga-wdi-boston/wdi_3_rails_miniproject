f2b9e6185c5560d2738823c0a8b883ad16066c15
cyurtbil <cenky92@gmail.com>
2014-10-09 16:01:26 -0400

    Refactored the post controller, now you can delete the posts from index page

--- Blog/app/controllers/posts_controller.rb
+++ Blog/app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+	before_action :set_post, only[:show, :edit, :update, :destroy]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -13,9 +14,19 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.create(post_params)
+		redirect_to @post
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to :back
 	end
 
 	private
+
+		def set_post
+			@post = Post.find(params[:id])
+		end
 
 		def post_params
 			params.require(:post).permit(:title, :author, :content)
