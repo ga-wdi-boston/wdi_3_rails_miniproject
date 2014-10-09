331150056a8f19b4a282d22ba51b07474006a9df
Billy1339 <william.m.sargent@gmail.com>
2014-10-09 15:36:11 -0400

    create new posts functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,29 @@ class PostsController < ActionController::Base
   end
 
   def show
+    @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(comment_params)
+    @post.save
+    redirect_to @post
+  end
+
+
 
   private
 
   def find_post
     @post = Post.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:post).permit(:title, :post)
   end
 
 end
