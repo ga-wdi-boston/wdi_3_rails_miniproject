7d26824b48514d777e4b5fe404d0efbae6e294a2
cyurtbil <cenky92@gmail.com>
2014-10-13 19:59:16 -0400

    Added error messages for validation purposes, and so the user cannot post an empty article

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+	validates :title, presence: true, on: :create
+	validates :author, presence: true, on: :create
+	validates :content, presence: true, on: :create
 end
