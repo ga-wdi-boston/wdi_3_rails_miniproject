23c5ba7f777553ddd93aa1c5df41c58953efcdfb
cyurtbil <cenky92@gmail.com>
2014-10-12 22:56:41 -0400

    Added a login page and registration page, and their auhtentications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 
+	before_action :set_user, only: [:show, :edit, :updare, :destroy]
+
 	def index
 		@users = User.all
 	end
 
 	def login
-		@user = User.find(user_params)
+		redirect_to posts_path
+	end
+
+	def show
 	end
 
 	def new
@@ -14,11 +19,19 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.create(user_params)
-		redirect_to posts_path
+		if @user.save
+			redirect_to posts_path
+		else
+			render :new
+		end
 	end
 
 
 	private
+
+		def set_user
+			@user = User.find(params[:id])
+		end
 
 		def user_params
 			params.require(:user).permit(:first_name, :last_name, :password, :email)
